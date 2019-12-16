c68c48db38a7ecb9b44e4710353e21ac2a4c118f
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-12-16 01:34:15 +0100

    Update lambda functions for tests

--- lambda/src/main/java/lambda/test/DeleteTest.java
+++ lambda/src/main/java/lambda/test/DeleteTest.java
@@ -1,21 +1,33 @@
 package lambda.test;
 
-import CommunicationWithApiGateway.LambdaHelper;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import lambda.Handler;
 import model.test.Test;
+import util.Response;
 
-public class DeleteTest extends LambdaHelper {
+import java.io.IOException;
+
+public class DeleteTest extends Handler<Test> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        Test test = getTestFormBody(input);
-        if (test.getId() != null) {
-            if (dynamoDBMapper.load(Test.class, test.getId()) != null) {
-                dynamoDBMapper.delete(test);
-                return getResponse(200, "Successfully deleted");
+    public Response handleRequest(Test input, Context context) {
+        if (input.getId() != null) {
+            if (getMapper().load(Test.class, input.getId()) != null) {
+                getMapper().delete(input);
+                return new Response(200, "Successfully deleted");
             }
-        } return getResponse(404, "Such test does not exist");
+        }
+        return new Response(404, "Such test does not exist");
     }
+
+//    @Override
+//    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+//        Test test = getTestFormBody(input);
+//        if (test.getId() != null) {
+//            if (dynamoDBMapper.load(Test.class, test.getId()) != null) {
+//                dynamoDBMapper.delete(test);
+//                return getResponse(200, "Successfully deleted");
+//            }
+//        } return getResponse(404, "Such test does not exist");
+//    }
 }
--- lambda/src/main/java/lambda/test/GetAllTests.java
+++ lambda/src/main/java/lambda/test/GetAllTests.java
@@ -1,27 +1,39 @@
 package lambda.test;
 
-import CommunicationWithApiGateway.LambdaHelper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lambda.Handler;
 import model.test.Test;
+import util.Response;
 
 import java.util.List;
 
-public class GetAllTests extends LambdaHelper {
+public class GetAllTests extends Handler<Object> {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        List<Test> tests = dynamoDBMapper.scan(Test.class, new DynamoDBScanExpression());
-        String result = null;
-        try {
-            result = objectMapper.writeValueAsString(tests);
-        } catch (JsonProcessingException e) {
-            result = "Could not get all tests";
+    public Response handleRequest(Object input, Context context) {
+        List<Test> tests = getMapper().scan(Test.class, new DynamoDBScanExpression());
+        if (tests != null && tests.size() != 0) {
+            return new Response(200, tests);
+        } else {
+            return new Response(404, "Could not get all tests");
         }
-        return getResponse(200, result);
     }
+
+
+//    @Override
+//    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+//        List<Test> tests = dynamoDBMapper.scan(Test.class, new DynamoDBScanExpression());
+//        String result = null;
+//        try {
+//            result = objectMapper.writeValueAsString(tests);
+//        } catch (JsonProcessingException e) {
+//            result = "Could not get all tests";
+//        }
+//        return getResponse(200, result);
+//    }
 }
 
 

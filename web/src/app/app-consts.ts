17dc189c7cb2c33168fcc74f9066977f95fb6658
4kaze <32jacke32@gmail.com>
2019-12-27 20:09:08 +0100

    add constants file to .gitignore

--- web/src/app/app-consts.ts
+++
@@ -1,4 +0,0 @@
-export const apiBaseUrl = 'https://unb3hured6.execute-api.us-east-1.amazonaws.com/test/applicant';
-export const userPoolId = 'us-east-1_l660ZcP9c';
-export const clientId = '1dbruefiorqcjfg0k8d0kt5gnh';
-export const recruiterIdentityPoolId = 'us-east-1:f44e8ba9-571f-49ff-a60c-6cf39d0bb245';

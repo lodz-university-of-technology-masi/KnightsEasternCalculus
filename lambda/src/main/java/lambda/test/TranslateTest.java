a4599a06bfd5ebffe248d40155556f4a780b5538
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2020-01-16 20:18:26 +0100

    Update TranslateTest.java

--- lambda/src/main/java/lambda/test/TranslateTest.java
+++ lambda/src/main/java/lambda/test/TranslateTest.java
@@ -10,11 +10,8 @@ public class TranslateTest extends Handler<Test> {
 
     @Override
     public Response handleRequest(Test input, Context context) {
-//        Test test = getMapper().load(Test.class, input.getTestId(), input.getTestId());
-//        if (test == null || test.getLanguage() != input.getLanguage()) {
-            Translator translator = new Translator(input);
-            input = translator.translateTest();
-//        }
+        Translator translator = new Translator(input);
+        input = translator.translateTest();
         return new Response(200, input);
     }
 }

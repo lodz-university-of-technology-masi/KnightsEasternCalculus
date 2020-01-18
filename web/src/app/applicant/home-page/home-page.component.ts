aef03c0bce59cf26a9ebf0b766afa8e4ec8bc6f9
4kaze <32jacke32@gmail.com>
2020-01-18 23:25:38 +0100

    add home pages

--- web/src/app/applicant/home-page/home-page.component.ts
+++ web/src/app/applicant/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TestInstance, TestStatus} from '../../model/test-instance';
+import {TestService} from '../../services/test.service';
+import {AuthenticationRecruiterService} from '../../services/authentication-recruiter.service';
+import {HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  private date: string;
+  private dayTime: string;
+  private testCount: number;
+
+  constructor(private testService: TestService, private authServive: AuthenticationRecruiterService) { }
 
   ngOnInit() {
+    this.testService.getTestInstances(this.authServive.getUserId()).subscribe(res => this.testCount = ((res as HttpResponse<TestInstance>).body as undefined as Array<TestInstance>)
+      .reduce((total, test) => {
+        if (test.status === TestStatus.NotSolved) {
+          return total + 1;
+        } else {
+          return total;
+        }
+    }, 0));
+    const date = new Date();
+    const format = {weekday: 'long', month: 'long', day: '2-digit', year: 'numeric'};
+    this.date = date.toLocaleDateString('pl-PL', format);
+    if (date.getHours() > 18) {
+      this.dayTime = 'wieczorne';
+    } else if(date.getHours() < 5) {
+      this.dayTime = 'nocne';
+    } else {
+      this.dayTime = 'dzienne';
+    }
   }
 
 }
--- web/src/app/recruiter/home-page/home-page.component.ts
+++ web/src/app/recruiter/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationRecruiterService} from '../../services/authentication-recruiter.service';
+import DateTimeFormatOptions = Intl.DateTimeFormatOptions;
+import {TestService} from '../../services/test.service';
+import {TestInstance} from '../../model/test-instance';
 
 @Component({
   selector: 'app-home-page',
@@ -8,11 +11,25 @@ import {AuthenticationRecruiterService} from '../../services/authentication-recr
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private authService: AuthenticationRecruiterService) {
-    console.log(this.authService.getAccessToken());
+  private date: string;
+  private dayTime: string;
+  private testCount: number;
+
+  constructor(private testService: TestService) {
   }
 
   ngOnInit() {
+    this.testService.getUncheckedTests().subscribe(res => this.testCount = (res.body as unknown as Array<TestInstance>).length)
+    const date = new Date();
+    const format = {weekday: 'long', month: 'long', day: '2-digit', year: 'numeric'};
+    this.date = date.toLocaleDateString('pl-PL', format);
+    if (date.getHours() > 18) {
+      this.dayTime = 'wieczorna';
+    } else if(date.getHours() < 5) {
+      this.dayTime = 'nocna';
+    } else {
+      this.dayTime = 'dzienna';
+    }
   }
 
 }
